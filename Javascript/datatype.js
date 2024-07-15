f714dde80e98cda8ad9ef30c0d5f97f5e2cf46d6
Arman <armanfaruque786@gmail.com>
2024-07-15 11:41:38 +0530

    datatype,operatoins,comparisions

---
+++ Javascript/datatype.js
@@ -0,0 +1,59 @@
+// let score = "33"
+// // console.log(score)
+// // console.log(typeof(score))
+
+// // let score1 = undefined
+// // console.log(score1)
+
+// let valueInNumber = Number(score) // by doing this we can convert srting into number
+// console.log(typeof valueInNumber)
+
+// let name  =  1233432
+// let nameInString = String(name)  //by doing this we can convert  number into string 
+// console.log(typeof nameInString)
+ 
+// let name1 = "arman"
+// let nameInNumber = Number(name1)
+// console.log(typeof nameInNumber)
+
+// let car = "234anfhf"
+// let carintonumber =  Number(car)
+// console.log(typeof carintonumber) // output will Nan 
+
+// let isLoggedIn = 1
+// let booleanLogedIn = Boolean(isLoggedIn)
+// console.log(booleanLogedIn) // true
+
+// let isLoggedIn1 = 0
+// let booleanLogedIn1 =  Boolean(isLoggedIn1)
+// console.log(booleanLogedIn1) // false
+ //  in Boolean 1 = true and 0 = false 
+
+
+ // OPERATIONS 
+
+//  let str =  "Arman"
+//  let str1 = " Bye"
+//  console.log(str + str1)
+//  console.log(1+9%8*3)
+
+// console.log(1+"3")   // 13
+// console.log("3" + 1) // 31
+// console.log(7 + 6 + "9")  // 139
+
+//     COMPARISON
+
+// console.log(1 === 1) // true
+// let a = 1
+// let b = 1
+// console.log(a === b) // true 
+
+// let c = null
+// let d = undefined
+// console.log(c === d) // false 
+
+// console.log(1 == 1) // true 
+// console.log( "1" ==  1) // true 
+// console.log("1" === 1)  // false because when we use  " === " this strictly check for datatype  if the datatype is same only then the comaparision we will be done
+
+
