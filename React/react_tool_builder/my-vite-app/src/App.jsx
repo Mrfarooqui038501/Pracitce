dd7ade19cf61ec01d913dcc9200ed9898dcd8a66
Arman <armanfaruque786@gmail.com>
2024-07-29 00:50:45 +0530

    about conditional rendering

--- React/react_tool_builder/my-vite-app/src/App.jsx
+++ React/react_tool_builder/my-vite-app/src/App.jsx
@@ -3,24 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MyComponent from './MyComponent.jsx'  // here we imported the MyComponent file 
-
+import Display from './Display.jsx'
+import ConditionalRendering from './ConditionalRendering.jsx'
 
 function App() {
+  const fruits = ["Apple","mango","orange","papaya"]
+  const profile = {name:'Arman',profile: 'dev'}
   
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <MyComponent message = "I am new" /> {/* here we are calling it */}
-      <MyComponent message = "I am going to learn react " /> {/* here we are calling it */}
-      <MyComponent message = "Hey is my changes visible" /> {/* here we are calling it */}
       
+      {/* <MyComponent message = "I am new" />  here we are calling it 
+      <MyComponent message = "I am going to learn react " /> here we are calling it 
+      <MyComponent message = "Hey is my changes visible" /> here we are calling it */}
+      {/* <h2> Props example with arrays</h2>
+      <Display fruits ={fruits} profile ={profile} /> */}
+      <ConditionalRendering isloggedin ={false} usename = {'Amman'} />
       
 
         
