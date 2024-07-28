078fc65ca7e3da7053fcff74fd5641f02ebed19f
Arman <armanfaruque786@gmail.com>
2024-07-28 23:59:03 +0530

    creating h1 and basics app of react

---
+++ React/react_tool_builder/my-vite-app/src/MyComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react"; // first we need to import the react 
+const MyComponent = () =>{   // here we made a callback functionn 
+    return <h1> Hello it's in react </h1>   // here we  created  h1 tag 
+    
+}
+ export default MyComponent;  /// here we are exporing it 
+
+ 
