3890d741b9b2de36c8d37c1e0a540c2972d331ca
Arman <armanfaruque786@gmail.com>
2024-07-29 23:48:35 +0530

    Counter app using use state

--- React/react_tool_builder/my-vite-app/src/App.jsx
+++ React/react_tool_builder/my-vite-app/src/App.jsx
@@ -6,10 +6,25 @@ import MyComponent from './MyComponent.jsx'  // here we imported the MyComponent
 import Display from './Display.jsx'
 import ConditionalRendering from './ConditionalRendering.jsx'
 
+
 function App() {
-  const fruits = ["Apple","mango","orange","papaya"]
-  const profile = {name:'Arman',profile: 'dev'}
-  
+  /*      Counter app 
+ //   const fruits = ["Apple","mango","orange","papaya"]
+ //   const profile = {name:'Arman',profile: 'dev'}
+
+  // we add counter app decremetn and increment button 
+  // and below we are setting button name counter and we are using useState 
+  let [counter, setCounter]  = useState(0); "setCounter" here we use "set" with "counter"  because we have set the variable first
+  //here this function is handling the decrement of count
+  const handleIncrement = () =>{   
+    setCounter(counter +1);
+  }
+  // here this function is handling the decrement of count 
+  const handleDecrement = () =>{  
+    setCounter(counter - 1);
+  }*/
+ 
+     
   return (
     <>
       
@@ -18,12 +33,18 @@ function App() {
       <MyComponent message = "Hey is my changes visible" /> here we are calling it */}
       {/* <h2> Props example with arrays</h2>
       <Display fruits ={fruits} profile ={profile} /> */}
-      <ConditionalRendering isloggedin ={false} usename = {'Amman'} />
-      
+      {/* <ConditionalRendering isloggedin ={false} usename = {'Amman'} /> */}
+      {/* <Button text='props clicked' handleClick={handleClick} /> */}
 
+      {/* <h1>Counter app.</h1>  here we give it a name counter app  */}
+      {/* <p>Count: {counter}</p>   and this will show the count as we using count variable here */}
+      {/* <button onClick={handleIncrement}>Increment</button> // here we made this increment button and using function on clik to increment  */}
+      {/* <button onClick={handleDecrement} >Decrement</button> // here we made this increment button and using function on clik to Decrement */}
+      
         
     </>
   )
+
 }
 
 export default App
